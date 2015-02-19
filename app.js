f07f95e96d83c8fe324e07bc116a10d1554a1031
unknown <a.begas@abegas-nb.kiev.ecofabric.com>
2015-02-19 17:42:46 +0100

    jst-1:  fixing rendering symbol counts, add disable event fot textarea

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var msg = document.querySelector('#js-message'),
     charLeft = document.querySelector('#js-message-left-symbols'),
     charLimit = 10;
 
-//clear textarea	
+//clear textarea
 msg.value = "";
 
 //set start limit value into html
@@ -13,15 +13,21 @@ charLeft.innerHTML = charLimit;
 //calculate symbols
 var calculateCharts = function(){
     var charCount = msg.value.length;
-	
-	msg.setAttribute('maxlength', charLimit);
-	
-	if(charCount > charLimit){
-		return false;
-	}
-	
-	charTotal.innerHTML = charCount;
-	charLeft.innerHTML  = charLimit - charCount
+    if(charCount  > charLimit){
+        return false;
+    }
+    charTotal.innerHTML = charCount;
+    charLeft.innerHTML  = charLimit - charCount;
 }
 
+//disable texting
+var disableCharts = function(event){
+    var charCount = msg.value.length;
+    var chartKeyCode = event.keyCode;
+    if(charCount >= charLimit && (chartKeyCode != 8 && chartKeyCode != 46 && chartKeyCode != 37 && chartKeyCode != 39)){
+        event.preventDefault();
+    }
+}
+
+msg.addEventListener("keydown", disableCharts);
 msg.addEventListener("keyup", calculateCharts);
