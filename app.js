f852f232d28fa943975a101f54376fc39d5debc5
unknown <a.begas@abegas-nb.kiev.ecofabric.com>
2015-02-24 17:40:50 +0100

    jst-3: create array with charts numbers

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 //set variables
-var msg = document.querySelector('#js-message'),
-    charTotal = document.querySelector('#js-message-left-total'),
-    charLeft = document.querySelector('#js-message-left-symbols'),
-    charLimit = 10;
-
-//clear textarea
-msg.value = "";
+var msg            = document.querySelector('#js-message'),
+    charTotal      = document.querySelector('#js-message-left-total'),
+    charLeft       = document.querySelector('#js-message-left-symbols'),
+    charLimit      = 10;
 
 //set start limit value into html
 charLeft.innerHTML = charLimit;
@@ -20,14 +17,22 @@ var calculateCharts = function(e){
     charLeft.innerHTML  = charLimit - charCount;
 }
 
-//disable texting
+//disable typing
 var disableCharts = function(event){
     var charCount = msg.value.length;
-    var chartKeyCode = event.keyCode;
-    if(charCount >= charLimit && (chartKeyCode != 8 && chartKeyCode != 46 && chartKeyCode != 37 && chartKeyCode != 39)){
+    var enableChars = [8, 16, 35, 36, 37, 38, 39, 40, 46];
+    for(i = 0; i< enableChars.length; i++){
+        if(event.keyCode == enableChars[i]){
+            var isAvailable = true;
+            break;
+        }
+    }
+
+    if(charCount >= charLimit && !isAvailable){
         event.preventDefault();
     }
 }
 
 msg.addEventListener("keydown", disableCharts);
 msg.addEventListener("keyup", calculateCharts);
+calculateCharts();
