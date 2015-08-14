453c984ce8dae3e3b4887e36195a0772df4dd21a
Alexey Begas <a.begas@astoundcommerce.com>
2015-08-14 15:58:45 +0200

    jst-5: OOP structure was added

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
+/**
+ * TextCounter
+ */
+var TextCounter = {
+    maxChar: 10,
+    textArea: null,
+    textAreaTotal: null,
+    textAreaLeft: null,
+    init: function() {
+        this.textArea = document.querySelector('#js-message');
+        this.textAreaTotal = document.querySelector('#js-message-left-total');
+        this.textAreaLeft = document.querySelector('#js-message-left-symbols');
+
+        this.events();
+    },
+
+    events: function() {},
+    // ... put you methods here
+};
+
+
 //set variables
-var msg            = document.querySelector('#js-message'),
-    charTotal      = document.querySelector('#js-message-left-total'),
-    charLeft       = document.querySelector('#js-message-left-symbols'),
+var textArea       = document.querySelector('#js-message'),
+    textAreaTotal      = document.querySelector('#js-message-left-total'),
+    textAreaLeft       = document.querySelector('#js-message-left-symbols'),
     charLimit      = 10;
 
 //set start limit value into html
-charLeft.innerHTML = charLimit;
+textAreaLeft.innerHTML = charLimit;
 
 //calculate symbols
 var calculateCharts = function(e){
-    var charCount = msg.value.length;
+    var charCount = textArea.value.length;
     if(charCount  > charLimit){
         e.preventDefault();
     }
-    charTotal.innerHTML = charCount;
-    charLeft.innerHTML  = charLimit - charCount;
+    textAreaTotal.innerHTML = charCount;
+    textAreaLeft.innerHTML  = charLimit - charCount;
 }
 
 //disable typing
 var disableCharts = function(event){
-    var charCount = msg.value.length;
+    var charCount = textArea.value.length;
     var enableChars = [8, 16, 35, 36, 37, 38, 39, 40, 46];
     for(i = 0; i< enableChars.length; i++){
         if(event.keyCode == enableChars[i]){
@@ -33,6 +54,6 @@ var disableCharts = function(event){
     }
 }
 
-msg.addEventListener("keydown", disableCharts);
-msg.addEventListener("keyup", calculateCharts);
+textArea.addEventListener("keydown", disableCharts);
+textArea.addEventListener("keyup", calculateCharts);
 calculateCharts();
